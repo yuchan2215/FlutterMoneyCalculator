98c94cb89aa73ec42593feedee8b6cffd6dc786c
Miyayu <yuchan2215@gmail.com>
2022-05-20 17:09:47 +0900

    [add] 入力する場所を作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,15 +31,91 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _sumMoney = 0;
+  final List<int> moneyTypes = [1, 5, 10, 50, 100, 1000, 2000, 5000, 10000];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: const Center(
-        child: Text("Hello World"),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(children: [
+            Expanded(
+                child: ListView.builder(
+                    itemCount: moneyTypes.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                          child: SizedBox(
+                        height: 50.0,
+                        child: inputItems(index),
+                      ));
+                    })),
+          ]),
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  ///入力するためのアイテムを取得する
+  Row inputItems(int index) {
+    return Row(children: [
+      SizedBox(
+          width: 90.0,
+          child: Text("${moneyTypes[index]}円",
+              textAlign: TextAlign.right,
+              style: const TextStyle(
+                fontSize: 20.0,
+              ))),
+      const SizedBox(width: 10.0),
+      SizedBox(
+          width: 40.0,
+          height: 40.0,
+          child: ElevatedButton(
+            onPressed: () {
+              //TODO 引くボタンが押された時のイベント
+            },
+            child: const Text("-", textAlign: TextAlign.center),
+          )),
+      const SizedBox(width: 10.0),
+      //入力
+      Expanded(
+        child: Center(
+          child: SizedBox(
+            height: 40.0,
+            child: TextField(
+              textAlignVertical: TextAlignVertical.center,
+              enabled: true,
+              maxLengthEnforcement: MaxLengthEnforcement.none,
+              maxLength: 10,
+              maxLines: 1,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              decoration: const InputDecoration(
+                  counterText: '',
+                  border: OutlineInputBorder(),
+                  contentPadding: EdgeInsets.only(
+                    left: 10,
+                    bottom: 20,
+                  )),
+            ),
+          ),
+        ),
+      ),
+      const SizedBox(width: 10.0),
+      SizedBox(
+          width: 40.0,
+          height: 40.0,
+          child: ElevatedButton(
+            onPressed: () {
+              //TODO 足すボタンが押された時のイベント
+            },
+            child: const Text("+", textAlign: TextAlign.center),
+          ))
+    ]);
+  }
+
   }
 }
